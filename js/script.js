0a3fbcc624775979a307248930e1c7aaa399e534
profJoey <137945663+profJoey@users.noreply.github.com>
2024-10-17 12:35:11 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const gallery = document.getElementById('tree-gallery');
+
+trees.forEach(tree => {
+    const treeDiv = document.createElement('div');
+    treeDiv.className = 'tree';
+
+    const treeName = document.createElement('h2');
+    treeName.textContent = tree.name;
+    treeDiv.appendChild(treeName);
+
+    const treeImage = document.createElement('img');
+    treeImage.src = tree.imagePath;
+    treeImage.alt = tree.name;
+    treeDiv.appendChild(treeImage);
+
+    const treeDescription = document.createElement('p');
+    treeDescription.textContent = `Description: ${tree.description}`;
+    treeDiv.appendChild(treeDescription);
+
+    const treeLink = document.createElement('a');
+    treeLink.href = tree.link;
+    treeLink.textContent = "Learn More";
+    treeDiv.appendChild(treeLink);
+
+    gallery.appendChild(treeDiv);
+});
